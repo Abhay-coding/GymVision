cc3b7d6dfaf6d462f0142a6cfb0c8e8701432b8f
Abhay-coding <abhay.kumar.8669@gmail.com>
2026-06-28 18:59:34 +0530

    Short descriptive commit message

--- MainPage/main.py
+++ MainPage/main.py
@@ -207,20 +207,9 @@ def main():
             video_processor_factory=VideoProcessorClass,
             rtc_configuration={
                 "iceServers": [
-                    {
-                        "urls": [
-                            "stun:stun.l.google.com:19302"
-                        ]
-                    },
-                    {
-                        "urls": [
-                            "turn:openrelay.metered.ca:80",
-                            "turn:openrelay.metered.ca:443",
-                            "turn:openrelay.metered.ca:443?transport=tcp"
-                        ],
-                        "username": "openrelayproject",
-                        "credential": "openrelayproject"
-                    }
+                {
+                    "urls": ["stun:stun.l.google.com:19302"]
+                }
                 ]
             },
             media_stream_constraints={
