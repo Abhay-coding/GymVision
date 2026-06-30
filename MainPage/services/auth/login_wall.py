b7fef853a423f75948cf241e001899d2a0b6dd38
Abhay-coding <abhay.kumar.8669@gmail.com>
2026-06-30 15:59:21 +0530

    Match Railway Streamlit version with local

--- MainPage/services/auth/login_wall.py
+++ MainPage/services/auth/login_wall.py
@@ -10,10 +10,7 @@ def render_login_wall():
 
     with st.form("login_form",clear_on_submit=False):
         username = st.text_input("Name (unique)",placeholder="Unique Name")
-        submit_button = st.form_submit_button(
-            "Start Session",
-            use_container_width=True
-        )
+        submit_button = st.form_submit_button("Start Session",width="stretch")
     
 
     if submit_button:
